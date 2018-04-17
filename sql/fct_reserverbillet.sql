4d1f546539b383ec634a71d8a94ef7021ed741ce
bertille-ddp <bduboscd@etu.utc.fr>
2018-04-17 12:17:28 +0200

    Réservation des billets

--- sql/fct_reserverbillet.sql
+++ sql/fct_reserverbillet.sql
@@ -59,17 +59,13 @@ RETURNS numeric(12,0)
 LANGUAGE 'plpgsql'
 AS $BODY$
 DECLARE
-    numero_existants numeric(12,0)[];
     nouveau_numero numeric(12,0);
 
 BEGIN
-    SELECT INTO numero_existants array_agg("NumeroCarte")  -- pour pouvoir vérifier l'unicité, on sélectionne les numéros déjà attribués
-    FROM "Voyageur";
-
     SELECT INTO nouveau_numero floor(random()*1000000000000);  -- le numéro doit être compris entre 0 et 1 000 000 000 000 pour avoir 12 chiffres
 
-    IF nouveau_numero = any(numero_existants) THEN  -- si le numéro tiré est déjà attribué, on appelle à nouveau la fonction
-        SELECT INTO nouveau_numero "genereCarte"();
+    IF EXISTS (SELECT "NumeroCarte" FROM "Voyageur" WHERE "NumeroCarte" = nouveau_numero) THEN
+        SELECT INTO nouveau_numero "genereCarte"();  -- si le numéro tiré est déjà attribué, on appelle à nouveau la fonction
     ELSE
         RETURN nouveau_numero;  -- on retourne ensuite le nouveau numéro
     END IF;
@@ -142,5 +138,80 @@ BEGIN
 
     RETURN true;  -- tout s'est bien passé
 
+END
+$BODY$;
+
+
+CREATE OR REPLACE FUNCTION "reserverBillet"(                -- on réserve des billets pour un même trajet
+	nbBillet integer,
+	voyageur integer,
+	assurance boolean,
+	moyenPaiement "MoyenPaiement",
+	trajetID integer,
+	dt Date,
+	cl "Classe")
+RETURNS integer[]
+    LANGUAGE 'plpgsql'
+AS $BODY$
+DECLARE
+    max_places integer;
+	num_re integer;
+    places_attribuees integer[];
+    place integer;
+    i integer;
+BEGIN
+    IF cl = '1' THEN
+        SELECT INTO max_places placesPrem             -- on cherche combien de places libres il reste dans le train
+        FROM "placesRestantes"(trajetID, dt);
+    ELSE
+        SELECT INTO max_places placesSec
+        FROM "placesRestantes"(trajetID, dt);
+    END IF;
+
+    IF max_places < nbBillet THEN
+        RAISE EXCEPTION 'Il ne reste plus assez de place dans le train % du %.', trajetID, dt;
+    END IF;
+
+    INSERT INTO "Reservation" ("Voyageur", "Assurance", "MoyenPaiement") VALUES
+    (voyageur, assurance, moyenPaiement);  -- on crée d'abord la réservation
+
+    SELECT INTO num_re "Id"  -- on trouve son numéro
+    FROM "Reservation"
+    ORDER BY "Id" DESC LIMIT 1;
+
+    IF cl = '1' THEN                                   -- on trouve la borne sup du numéro de place qu'on pourra affecter
+        SELECT INTO max_places "nbPlacesPrem"
+        FROM "Trajet" INNER JOIN "Ligne"
+        ON "Trajet"."Ligne" = "Ligne"."Id"
+        INNER JOIN "TypeTrain"
+        ON "Ligne"."TypeTrain" = "TypeTrain"."Nom"
+        WHERE "Trajet"."Id" = trajetID;
+    ELSE
+        SELECT INTO max_places "nbPlacesSec"
+        FROM "Trajet" INNER JOIN "Ligne"
+        ON "Trajet"."Ligne" = "Ligne"."Id"
+        INNER JOIN "TypeTrain"
+        ON "Ligne"."TypeTrain" = "TypeTrain"."Nom"
+        WHERE "Trajet"."Id" = trajetID;
+    END IF;
+
+	FOR i IN 1..nbBillet LOOP                       -- pour chaque billet à prendre
+        SELECT INTO place floor(random()*max_places);  -- on tire un numéro de place inférieur à la borne sup
+
+        WHILE EXISTS (SELECT "Place" FROM "Billet" WHERE "Trajet" = trajetID AND "Date" = dt AND "Place" = place) LOOP -- on vérifie qu'il n'est pas déjà attribué
+            SELECT INTO place floor(random()*max_places);
+        END LOOP;
+
+        SELECT INTO places_attribuees array_append(places_attribuees, place);  -- on construit le tableau des places atribuées
+
+		INSERT INTO "Billet" ("Trajet", "Date", "Classe", "Place", "Annule", "Reservation") VALUES
+        (trajetID, dt, cl, places_attribuees[i], false, num_re);  -- on insère les valeurs dans la table
+	END LOOP;
+
+    IF NOT "updateStatut"(voyageur) THEN        -- on met à jour jour le statut du voyageur (qui passe peut-être or avec cette nouvelle réservation)
+        RAISE EXCEPTION 'Erreur à la mise à jour du statut du voyageur %.', voyageur;
+    END IF;
+
+    RETURN places_attribuees;        -- on renvoie les places prises
 END
 $BODY$;
